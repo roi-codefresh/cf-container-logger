05438d89d50f036c79564186eab3e275e26e5bb9
Itai Gendler <itai@codefresh.io>
2016-09-19 17:59:04 +0300

    update isReady script

--- lib/isReady.sh
+++ lib/isReady.sh
@@ -1,1 +1,12 @@
-bash -c "grep -q 'ready' ./state.json"
+#!/bin/bash
+#
+
+CONTAINER_ID=$1
+
+if [[ -n "$CONTAINER_ID" ]]; then
+   grep -q $CONTAINER_ID ./lib/state.json
+else
+  grep -q "ready" ./lib/state.json
+fi
+
+
