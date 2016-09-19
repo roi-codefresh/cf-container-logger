e3c2e93fff01dca22b8add326a4e166d4d3a78b6
Itai Gendler <itai@codefresh.io>
2016-09-19 12:15:44 +0300

    add codefresh.yml and tests

---
+++ test/logger.unit.spec.js
@@ -0,0 +1,31 @@
+/*var proxyquire = require('proxyquire').noCallThru();
+var Q          = require('q');
+var chai       = require('chai');
+var expect     = chai.expect;
+var sinon      = require('sinon');
+var sinonChai  = require('sinon-chai');
+chai.use(sinonChai);*/
+
+describe('Logger tests', function () {
+
+    describe('test', () => {
+
+        describe('positive tests', () => {
+
+            it('test', () => {
+
+            });
+
+        });
+
+        describe('negative tests', () => {
+
+            it('test', () => {
+
+            });
+
+        });
+
+    });
+
+});
