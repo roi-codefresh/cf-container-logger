11a41a48836543539ef7b78438e56bf4d977210f
Alexei Ledenev <alexei.led@gmail.com>
2017-04-12 12:35:52 +0300

    reduce image size; add init system; fix script to be POSIX compliant; fix package dependency

--- lib/isReady.sh
+++ lib/isReady.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 #
 
 CONTAINER_ID=$1
 
 echo "checking if container:$CONTAINER_ID exists"
-if [[ -n "$CONTAINER_ID" ]]; then
+if [ -n "$CONTAINER_ID" ]; then
    grep -q $CONTAINER_ID ./lib/state.json
 else
    grep -q "ready" ./lib/state.json
